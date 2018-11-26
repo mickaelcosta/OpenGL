36be666bce71c3ffa4df444022626523fd8897fb
User <user@macbook-pro-de-user-4.local>
2018-11-26 02:36:55 -0200

    Initial Commit

---
+++ aprendizado/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  aprendizado
+//
+//  Created by User on 26/11/18.
+//  Copyright © 2018 mickael. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
